802ae6626fef8aa9b50b7ba674afcb1796de30d8
santco <luis.garcia@gopycloud.com>
2021-04-08 13:48:13 -0400

    main,se verifican las rutas

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('logueo') }}">
+                    <form method="POST" action="{{ route('/loguear') }}">
                         @csrf
 
                         <div class="form-group row">
