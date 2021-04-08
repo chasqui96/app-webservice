0bca2ac36b01b40699e1a7c30367ca11b5a7732c
santco <luis.garcia@gopycloud.com>
2021-04-08 12:32:38 -0400

    main,se verifican las rutas

--- app/Models/Personal.php
+++ app/Models/Personal.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Foundation\Auth\Personal as Authenticatable;
+
 use Illuminate\Notifications\Notifiable;
 class Personal extends Authenticatable
 {
