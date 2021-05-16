237a4439747dcd25d62e9dcb5c14530325c14bda
santco <luis.garcia@gopycloud.com>
2021-05-16 10:54:50 -0400

    main,se cambio el protocologo de la ape

--- routes/api.php
+++ routes/api.php
@@ -51,4 +51,4 @@ Route::post('/CambiarEstadoPersonales', [App\Http\Controllers\PersonalController
 /// ------AGENDAMIENTO-CUPOS-----------------------------
 Route::get('/ListarAgendamiento', [App\Http\Controllers\PersonalController::class, 'index'])->name('agendamiento.index');
 Route::get('/ListarDoctor', [App\Http\Controllers\PersonalController::class, 'listarDoctor'])->name('personals.doctor');
-Route::get('/GuardarAgendamiento', [App\Http\Controllers\AgendamientoController::class, 'store'])->name('agendamiento.store');
+Route::post('/GuardarAgendamiento', [App\Http\Controllers\AgendamientoController::class, 'store'])->name('agendamiento.store');
