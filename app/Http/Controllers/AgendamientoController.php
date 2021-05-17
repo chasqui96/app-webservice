8742f7e352def1dcbf0dad130076367ac53df375
Chasqui <luisfernandogarciapenha@gmail.com>
2021-05-16 21:02:23 -0400

    main,nuevos cambios

--- app/Http/Controllers/AgendamientoController.php
+++ app/Http/Controllers/AgendamientoController.php
@@ -18,8 +18,32 @@ class AgendamientoController extends Controller
      */
     public function index()
     {
-        $users = Agendamiento::join('cupos', 'cupos.agendamiento_id', '=', 'agendamientos.id');
-        return $users;
+        //
+        $agendamientos  = Agendamiento::join('cupos','cupos.agendamiento_id','=','agendamientos.id') ->get(['agendamientos.*']);;
+        //dd($agendamientos);
+        $pacear = [];
+        $horas = [];
+        $conteo = 0;
+        foreach ($agendamientos as  $value) {
+             $id = $value->id;
+             $pacear[$conteo]['id'] = $value->id;
+             $pacear[$conteo]['hora_desde'] = $value->hora_desde;
+             $pacear[$conteo]['hora_hasta'] = $value->hora_hasta;
+             $pacear[$conteo]['dias'] = $value->dias;
+             $pacear[$conteo]['doc_registro'] = $value->doc_registro;
+             $pacear[$conteo]['espe_id'] = $value->espe_id;
+             $pacear[$conteo]['per_id'] = $value->per_id;
+             $cupos = Cupo::where("agendamiento_id","=",$id)->get();
+             $i=0;
+             foreach ($cupos as  $value2) {
+                $horas[$i]['cupo'] = $value2->horas;
+                $i++;
+             }
+             $pacear[$conteo]['horas'] = $horas;
+            
+            
+         }
+        return $pacear;
     }
 
     /**
@@ -71,13 +95,11 @@ class AgendamientoController extends Controller
             $horasG->reservados = 0;
             $horasG->fecha_cupos = date("Y-m-d");;
             $horasG->save();
-           $i+=1;
+            $i+=1;
             
         }
-
-       // dd($horas);
       
-         return $agendamientos;
+        return $agendamientos;
 
         return response()->json([
         'status'  => 500,
